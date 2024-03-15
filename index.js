4e1714df0c76adbaab1a34c029e11a6c12df414d
Ninja Spmc <146399329+ninjaspmc@users.noreply.github.com>
2024-03-15 08:58:08 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,28 @@
+bla = process.cwd()
+__path = process.cwd()
+//=>=>=>=>=>=>=>=[@]=<=<=<=<=<=<=<=<=//
+// const PORT = process.env.PORT || 4012
+//=>=>=>=>=>=>=>=[@]=<=<=<=<=<=<=<=<=//
+const express = require('express')
+const path = require("path");
+const { dirname } = require('path');
+var __dirname = dirname(__filename);
+
+const router = express.Router();
+var upload = multer()
+var app = express()
+ 
+app.use(express.static(__dirname));
+
+app.get("/", (res, next) => {
+res.sendFile(path.join(__dirname, "./inicio.html"))
+})
+app.use(router);
+
+app.get('/politicadecadastro',(req, res) => {
+res.sendFile(path.join(__dirname, "./politica-de-cadastro.html"))})
+
+//////MOSTRA SE O APP FOI ABERTO
+/* app.listen(PORT, () => {
+console.log('Aplicativo aberto na porta: ' + PORT)
+})
